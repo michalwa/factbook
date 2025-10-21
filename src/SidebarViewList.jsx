2db2fc54a0175e020e15f0c58c19bd102f25a435
michalwa <michalwa2003@gmail.com>
2025-10-21 02:04:43 +0200

    pretty rounded corners

--- src/SidebarViewList.jsx
+++ src/SidebarViewList.jsx
@@ -17,6 +17,10 @@ export default function SidebarViewList(props) {
               {view.name}
               <span class="badge">{view.entryCount}</span>
             </a>
+            <Show when={view.id === props.selectedId}>
+              <div class="sidebar-view-list-item-corner-top"></div>
+              <div class="sidebar-view-list-item-corner-bottom"></div>
+            </Show>
           </li>
         )}
       </For>
