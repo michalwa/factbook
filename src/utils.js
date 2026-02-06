cf64681df9f2283715a8b09df3bed437ec65c488
michalwa <michalwa2003@gmail.com>
2026-02-06 20:18:09 +0100

    remove dead code

--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,4 @@
 /**
- * Evaluates `f(x)` only if `x` is truthy. Otherwise returns `x`.
- *
- * @template T, U
- * @param {(_: T) => U} f
- * @param {T | null | undefined} x
- * @returns {U | null | undefined}
- */
-export function maybe(f, x) {
-  return x && f(x);
-}
-
-/**
- * Generates an array of sliding windows of length 3 across the input array,
- * where the first and last windows include a trailing `undefined` element,
- * such that for each window the middle element is a subsequent element of
- * the input array: `lookaround(xs)[i][1] === xs[i]`.
- *
- * @template T
- * @param {T[]} xs
- * @returns {[T | undefined, T | undefined, T | undefined][]}
- */
-export function lookaround(xs) {
-  return xs.map((_, i) => Array.from({ length: 3 }, (_, j) => xs[i + j - 1]));
-}
-
-/**
  * @typedef {("shift" | "ctrl" | "alt")} Modifier
  */
 
