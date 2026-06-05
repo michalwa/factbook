b05c43e131152a8e84021c4e9409566f29ca3fe2
michalwa <michalwa2003@gmail.com>
2026-06-05 19:05:14 +0200

    install system deps for swipl build

--- build-deps.sh
+++ build-deps.sh
@@ -4,11 +4,37 @@ set -e
 
 SWIPL_VERSION=10.0.2
 
+if [[ -n "$CI" ]]; then
+  apt update
+  apt install \
+    build-essential \
+    cmake \
+    ninja-build \
+    pkg-config \
+    ncurses-dev \
+    libedit-dev \
+    libgoogle-perftools-dev \
+    libgmp-dev \
+    libssl-dev \
+    unixodbc-dev \
+    zlib1g-dev \
+    libarchive-dev \
+    libossp-uuid-dev \
+    libdb-dev \
+    libpcre2-dev \
+    libyaml-dev \
+    libutf8proc-dev \
+    python3 \
+    libpython3-dev \
+    default-jdk \
+    junit4
+fi
+
 mkdir -p deps
 pushd deps
 
-if [ ! -d deps/swipl ]; then
-  if [ ! -f swipl-$SWIPL_VERSION.tar.gz ]; then
+if ! [[ -d deps/swipl ]]; then
+  if ! [[ -f swipl-$SWIPL_VERSION.tar.gz ]]; then
     wget https://www.swi-prolog.org/download/stable/src/swipl-$SWIPL_VERSION.tar.gz
   fi
 
