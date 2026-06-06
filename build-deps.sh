0f296d107361fe034be0a5cfa311de11830ca754
michalwa <michalwa2003@gmail.com>
2026-06-06 19:43:44 +0200

    check if exists before building deps

--- build-deps.sh
+++ build-deps.sh
@@ -4,49 +4,58 @@ set -e
 
 SWIPL_VERSION=10.0.2
 
-if [[ -n "$CI" ]]; then
-  apt update
-  apt install \
-    build-essential \
-    cmake \
-    ninja-build \
-    pkg-config \
-    ncurses-dev \
-    libedit-dev \
-    libgoogle-perftools-dev \
-    libgmp-dev \
-    libssl-dev \
-    zlib1g-dev \
-    libarchive-dev \
-    libossp-uuid-dev \
-    libdb-dev \
-    libpcre2-dev \
-    libyaml-dev \
-    libutf8proc-dev
-fi
+function pushd {
+  command pushd "$@" > /dev/null
+}
+
+function popd {
+  command popd "$@" > /dev/null
+}
 
 mkdir -p deps
 pushd deps
 
-if ! [[ -d deps/swipl ]]; then
-  if ! [[ -f swipl-$SWIPL_VERSION.tar.gz ]]; then
-    wget https://www.swi-prolog.org/download/stable/src/swipl-$SWIPL_VERSION.tar.gz
+if ! [[ -f swipl/build/src/swipl ]]; then
+  echo "Building swipl ($SWIPL_VERSION) from source..."
+
+  if [[ -n "$CI" ]]; then
+    apt update
+    apt install \
+      build-essential \
+      cmake \
+      ninja-build \
+      pkg-config \
+      ncurses-dev \
+      libedit-dev \
+      libgoogle-perftools-dev \
+      libgmp-dev \
+      libssl-dev \
+      zlib1g-dev \
+      libarchive-dev \
+      libossp-uuid-dev \
+      libdb-dev \
+      libpcre2-dev \
+      libyaml-dev \
+      libutf8proc-dev
   fi
 
+  wget https://www.swi-prolog.org/download/stable/src/swipl-$SWIPL_VERSION.tar.gz
   mkdir -p swipl
   tar -xzf swipl-$SWIPL_VERSION.tar.gz --strip-components=1 -C swipl
-fi
 
-pushd swipl
-rm -rf build
-mkdir build
-pushd build
-cmake -DCMAKE_BUILD_TYPE=PGO \
-  -DSWIPL_STATIC_LIB=ON \
-  -DBUILD_TESTING=OFF \
-  -DBUILD_SWIPL_LD=OFF \
-  -DINSTALL_DOCUMENTATION=OFF \
-  -G Ninja ..
-ninja
-popd
-popd
+  pushd swipl
+  mkdir -p build
+  pushd build
+  cmake \
+    -DCMAKE_BUILD_TYPE=PGO \
+    -DSWIPL_STATIC_LIB=ON \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_SWIPL_LD=OFF \
+    -DINSTALL_DOCUMENTATION=OFF \
+    -G Ninja ..
+  ninja
+  popd
+  popd
+else
+  echo "Found swipl, delete deps/swipl to force rebuild"
+fi
