6a5925c1440d2b77c1d44b2ea5847bf66483d522
michalwa <michalwa2003@gmail.com>
2026-06-06 20:50:56 +0200

    try suppress all clippy for generated module

--- factbook-swipl/src/bindings.rs
+++ factbook-swipl/src/bindings.rs
@@ -2,6 +2,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(unused)]
-#![allow(clippy::useless_transmute)]
+#![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
