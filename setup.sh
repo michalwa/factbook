3ee0f02a1a9ae58c25b285510435fab661514ee3
michalwa <michalwa2003@gmail.com>
2026-06-07 19:53:08 +0200

    fixes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-TARGET=${TARGET:-x86_64-unknown-linux-gnu}
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  SHARED_OBJ_EXT=".so"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  SHARED_OBJ_EXT=".dylib"
-fi
-
 if command -v apt; then
   sudo apt-add-repository ppa:swi-prolog/stable
   sudo apt update
@@ -28,19 +20,26 @@ else
   exit 1
 fi
 
-if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  SHARED_LIBS_EXT=".so"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  SHARED_LIBS_EXT=".dylib"
+fi
+
+if [[ -n "$SHARED_LIBS_TARGET" ]]; then
   mkdir -p libs
 
   function copy-lib {
     local source_path="$1/$2"
-    local target_path="libs/$2-${TARGET}"
+    local target_path="libs/$2-${SHARED_LIBS_TARGET}"
     echo "Copying ${source_path} -> ${target_path}"
     cp "$source_path" "$target_path"
   }
 
-  libswipl_dir=${libswipl_dir:-$(ldconfig -p | grep "libswipl${SHARED_OBJ_EXT}$" | sed 's/^.*=> //' | xargs dirname)}
+  libswipl_dir=${libswipl_dir:-$(ldconfig -p | grep "libswipl${SHARED_LIBS_EXT}$" | sed 's/^.*=> //' | xargs dirname)}
   libswipl_dir=${libswipl_dir:-$(pkg-config --libs-only-L swipl | tr -d ' ' | sed 's/-L//')}
-  copy-lib "$libswipl_dir" libswipl${SHARED_OBJ_EXT}
+  copy-lib "$libswipl_dir" libswipl${SHARED_LIBS_EXT}
 else
   echo "Unrecognized OSTYPE: ${OSTYPE}, skipped importing shared libs"
+  exit 1
 fi
