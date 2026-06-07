9606b79677771561f1a2650dfa0412c33af09dde
michalwa <michalwa2003@gmail.com>
2026-06-07 18:52:56 +0200

    fix windows path

--- setup.ps1
+++ setup.ps1
@@ -1,17 +1,13 @@
-mkdir -Force libs | Out-Null
+mkdir -Force libs
 
-echo "Downloading swipl-10.0.2-1.x64.exe..."
-$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest https://www.swi-prolog.org/download/stable/bin/swipl-10.0.2-1.x64.exe `
   -OutFile libs\swipl-installer.exe
 
-echo "Extracting..."
-7z x -y libs\swipl-installer.exe '-olibs\swipl' | Out-Null
-
+7z x -y libs\swipl-installer.exe '-olibs\swipl'
 rm libs\swipl-installer.exe
 
-if (Test-Path libs\swipl\bin\swipl.exe) {
-  echo "Done"
+if (Test-Path 'libs\swipl\bin\swipl.exe') {
+  echo "Successfully extracted swipl binaries"
 } else {
   echo "Extracted installer did not contain the expected files"
   exit 1
