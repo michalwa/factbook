d364f24158ee26b904afa5dde94bc43cf94b7f5d
michalwa <michalwa2003@gmail.com>
2026-06-07 19:28:38 +0200

    debug swipl not in path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-ENV_OUTPUT=${ENV_OUTPUT:-/dev/null}
 TARGET=${TARGET:-x86_64-unknown-linux-gnu}
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -29,24 +28,19 @@ else
   exit 1
 fi
 
-mkdir -p libs
-
-function copy-lib {
-  local source_path="$1/$2"
-  local target_path="libs/$2-${TARGET}"
-  echo "Copying ${source_path} -> ${target_path}"
-  cp "$source_path" "$target_path"
-}
-
-
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
-  # Echo `SWIPL` env var for the `swipl-info` crate. On some platforms there seems
-  # to be an issue with `swipl` not being added to `PATH`.
-  echo "SWIPL=$(which swipl)" | tee "$ENV_OUTPUT"
+  mkdir -p libs
+
+  function copy-lib {
+    local source_path="$1/$2"
+    local target_path="libs/$2-${TARGET}"
+    echo "Copying ${source_path} -> ${target_path}"
+    cp "$source_path" "$target_path"
+  }
 
   libswipl_dir=${libswipl_dir:-$(ldconfig -p | grep "libswipl${SHARED_OBJ_EXT}$" | sed 's/^.*=> //' | xargs dirname)}
   libswipl_dir=${libswipl_dir:-$(pkg-config --libs-only-L swipl | tr -d ' ' | sed 's/-L//')}
   copy-lib "$libswipl_dir" libswipl${SHARED_OBJ_EXT}
 else
-  echo "Unrecognized OSTYPE: ${OSTYPE}, skipped searching for swipl"
+  echo "Unrecognized OSTYPE: ${OSTYPE}, skipped importing shared libs"
 fi
