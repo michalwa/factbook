f60a8949c5c54d215392967e2422f2805707efbd
michalwa <michalwa2003@gmail.com>
2026-06-07 17:42:10 +0200

    restore import-libs

--- import-libs.sh
+++ import-libs.sh
@@ -7,8 +7,11 @@ set -e
 IMPORTED_LIBS_PATH=libs
 IMPORTED_LIBS_TARGET=x86_64-unknown-linux-gnu
 
+# import-lib <pkg-name> <lib-name> <lib-path>
 function import-lib {
-  local path=$(ldconfig -p | grep "$1$" | sed 's/^.*=> //')
+  local path="$3"
+  path=${path:-$(ldconfig -p | grep "$2$" | sed 's/^.*=> //')}
+  path=${path:-$(pkg-config --libs-only-L $1 | tr -d ' ' | sed 's/-L//')/$2}
 
   if ! [[ -f "$path" ]]; then
     echo "Shared library missing: $1"
@@ -23,4 +26,4 @@ function import-lib {
 }
 
 mkdir -p ${IMPORTED_LIBS_PATH}
-import-lib libswipl.so
+import-lib swipl libswipl.so ${LIBSWIPL_PATH}
