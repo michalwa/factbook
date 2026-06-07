7a8744172c1d8174274e59156d365956e0097361
michalwa <michalwa2003@gmail.com>
2026-06-07 14:01:47 +0200

    draft release action

--- import-libs.sh
+++ import-libs.sh
@@ -4,13 +4,14 @@
 
 set -e
 
-IMPORTED_PATH=libs
+IMPORTED_LIBS_PATH=libs
+IMPORTED_LIBS_TARGET=x86_64-unknown-linux-gnu
 
 function import-lib {
-    local imported_path="${IMPORTED_PATH}/$(basename $1)"
+    local imported_path="${IMPORTED_LIBS_PATH}/$(basename $1)-${IMPORTED_LIBS_TARGET}"
     echo "Copying $1 -> ${imported_path}"
     cp $1 ${imported_path}
 }
 
-mkdir -p $IMPORTED_PATH
+mkdir -p $IMPORTED_LIBS_PATH
 import-lib ${LIBSWIPL_PATH:-$(ldconfig -p | grep libswipl.so$ | sed 's/^.*=> //')}
